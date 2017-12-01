b36aeacd37d7ad276c5a5fcad2162dea0e679322
pranavchaphekar <chaphekarpranav@gmail.com>
2017-12-01 17:13:12 -0500

    Added few comments

--- src/ADBFinalProject/TransactionManager.java
+++ src/ADBFinalProject/TransactionManager.java
@@ -6,10 +6,23 @@ class TransactionManager {
 
   private Map<Integer, Transaction> runningTransactions;
 
+  /**
+   * Adds the transaction to the running Transactions map to keep a track of currently running
+   * transactions
+   *
+   * @param tId transaction Id
+   * @param transaction {@code Transaction}
+   */
   void addTransaction(Integer tId, Transaction transaction) {
     runningTransactions.put(tId, transaction);
   }
 
+  /**
+   * Deletes the transaction from the running Transaction map as soon as the transaction is ended
+   *
+   * @param tId transaction Id
+   * @return true if deletion is successful, false otherwise
+   */
   boolean deleteTransaction(Integer tId) {
     if (runningTransactions.containsKey(tId)) {
       runningTransactions.remove(tId);
