47569fccac7f7b0ae0242ccb89f068be9bfbae7e
pranavchaphekar <chaphekarpranav@gmail.com>
2017-12-03 16:50:05 -0500

    Formatted Operations class

--- src/ADBFinalProject/Operation.java
+++ src/ADBFinalProject/Operation.java
@@ -1,6 +1,8 @@
 package ADBFinalProject;
 
 /**
+ * Contains the details of the operation (operation can be read or write)
+ *
  * @author Sanchit Mehta, Pranav Chaphekar
  * @see Site
  * @see Transaction
@@ -25,7 +27,7 @@ class Operation {
   Operation(int transactionId, int variableId, boolean isWriteOperation) {
     this.transactionId = transactionId;
     this.variableId = variableId;
-    this.isWriteOperation = false;
+    this.isWriteOperation = isWriteOperation;
     isEndOperation = false;
   }
 
@@ -33,18 +35,56 @@ class Operation {
     isEndOperation = false;
   }
 
-  public boolean isEndOperation() {
+  /**
+   * Checks if the operation has ended
+   *
+   * @return true if the operation has ended, false otherwise
+   */
+  boolean isEndOperation() {
     return isEndOperation;
   }
 
-  public int getTransactionId() {
+  /**
+   * Checks if the operation is a write operation
+   *
+   * @return true if it is a write operation; false otherwise
+   */
+  boolean isWriteOperation() {
+    return isWriteOperation;
+  }
+
+  /**
+   * Sets the flag for the read operation
+   *
+   * @return true if the operation is a read operation, false otherwise
+   */
+  boolean isReadOperation() {
+    return !isWriteOperation && !isEndOperation;
+  }
+
+  /**
+   * Getter method for transaction Id
+   *
+   * @return transaction Id
+   */
+  int getTransactionId() {
     return transactionId;
   }
 
+  /**
+   * Getter method for variable Id
+   *
+   * @return variable Id
+   */
   int getVariableId() {
     return variableId;
   }
 
+  /**
+   * Getter method for the value of the variable
+   *
+   * @return variable value
+   */
   int getVariableVal() {
     if (!isWriteOperation) {
       throw new UnsupportedOperationException();
@@ -52,12 +92,14 @@ class Operation {
     return variableVal;
   }
 
-  boolean isWriteOperation() {
-    return isWriteOperation;
+  @Override
+  public String toString() {
+    return "Operation{" +
+        "transactionId=" + transactionId +
+        ", variableId=" + variableId +
+        ", variableVal=" + variableVal +
+        ", isWriteOperation=" + isWriteOperation +
+        ", isEndOperation=" + isEndOperation +
+        '}';
   }
-
-  boolean isReadOperation() {
-    return !isWriteOperation && !isEndOperation;
-  }
-
 }
