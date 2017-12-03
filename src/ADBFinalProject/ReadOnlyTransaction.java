9779f139fe4d0d4759c564c8b53261559c01a3e9
pranavchaphekar <chaphekarpranav@gmail.com>
2017-12-03 01:55:29 -0500

    Added Implementation of Read Only Transaction

--- src/ADBFinalProject/ReadOnlyTransaction.java
+++ src/ADBFinalProject/ReadOnlyTransaction.java
@@ -1,8 +1,68 @@
 package ADBFinalProject;
 
-public class ReadOnlyTransaction extends Transaction {
+/**
+ * Read Only Transaction takes the snapshot of the system that has been last committed before this
+ * transaction begins
+ */
+class ReadOnlyTransaction extends Transaction {
 
-  public ReadOnlyTransaction(int transactionId, int startTime) {
+  private Integer[] variableValuesAtStart;
+  private static final int NUMBER_OF_VARIABLES = 20;
+  private Site[] sites;
+
+  ReadOnlyTransaction(int transactionId, int startTime, Site[] sites) {
     super(transactionId, startTime);
+    this.sites = sites;
+    variableValuesAtStart = new Integer[NUMBER_OF_VARIABLES + 1];
+    populateMap();
   }
+
+  @Override
+  public String toString() {
+    return "ReadOnlyTransaction{" + super.getId() + "}";
+  }
+
+  /**
+   * Getter method for the variable values at the start
+   *
+   * @return Integer array containing the variable values
+   */
+  Integer[] getVariableValuesAtStart() {
+    return this.variableValuesAtStart;
+  }
+
+  /**
+   * Gets the value of the variable
+   *
+   * @param variableIndex the index of the variable
+   * @return the value of the variable at that index; null otherwise
+   */
+  Integer getVariableValue(int variableIndex) {
+    if (variableIndex < variableValuesAtStart.length) {
+      return variableValuesAtStart[variableIndex];
+    } else {
+      return null;
+    }
+  }
+
+  /**********************************
+   Private Helper Methods
+   **********************************/
+
+  private void populateMap() {
+    for (int variableIdx = 1; variableIdx < variableValuesAtStart.length; variableIdx++) {
+      for (int siteIdx = 1; siteIdx < sites.length; siteIdx++) {
+        if (sites[siteIdx].isRunning()
+            && sites[siteIdx].getIndexToVarMap().containsKey(variableIdx)) {
+          variableValuesAtStart[variableIdx] =
+              sites[siteIdx]
+                  .getIndexToVarMap()
+                  .get(variableIdx)
+                  .getVal();
+          break;
+        }
+      }
+    }
+  }
+
 }
